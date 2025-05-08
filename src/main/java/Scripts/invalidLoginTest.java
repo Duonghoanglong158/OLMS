5e9ec8160f1787593c4df18a8743c875cab62318
Duonghoanglong158 <duonghoanglong1158@gmail.com>
2025-05-08 20:59:09 +0700

    add project

---
+++ src/main/java/Scripts/invalidLoginTest.java
@@ -0,0 +1,43 @@
+package Scripts;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import Pages.HomePage;
+import Pages.LoginPage;
+
+public class invalidLoginTest extends BaseTest{
+
+    @Test
+    public void invalidBlankPasswordTest(){
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+
+        homePage.clickSignInButton();
+        //Nhập username
+        //click login button
+        loginPage.login("huydao226","");
+
+        //Verify if there is error message displays
+        loginPage.verifyErrorMessageIsDisplay();
+    }
+
+    @Test
+    public void invalidUsernameTest(){
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+
+        homePage.clickSignInButton();
+        //Nhập username
+        //click login button
+        loginPage.login("dasdsadasdasdasdadsda","adasdsadasdasda");
+
+        //Verify if there is error message displays
+        loginPage.verifyErrorMessageIsDisplay();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
+
